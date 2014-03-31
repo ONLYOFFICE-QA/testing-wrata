18b8a4e803cdad0915895a4abdd49e1c5634f6b6
facetostool <ivan.tugin@gmail.com>
2014-03-31 23:49:30 +0100

    add buttons for update threads without restart in admin panel

--- app/server/server_threads.rb
+++ app/server/server_threads.rb
@@ -28,6 +28,33 @@ class ServerThreads < ActionController::Base
     servers
   end
 
+  def update_models
+    servers = Server.all.sort_by {|s| s.name.split('nct-at')[1].to_i}
+    @server_threads.each_with_index do |server_thread, i|
+      server_thread.change_model(servers[i])
+    end
+  end
+
+  def add_threads
+    new_servers = Server.all.sort_by {|s| s.name.split('nct-at')[1].to_i}
+    old_servers = @server_threads.inject([]) {|arr, thread| arr << thread.server_model }
+    difference = new_servers - old_servers
+    difference.each do |server|
+      @server_threads << ServerThread.new(server)
+    end
+  end
+
+  def delete_threads
+    new_servers = Server.all.sort_by {|s| s.name.split('nct-at')[1].to_i}
+    old_servers = @server_threads.inject([]) {|arr, thread| arr << thread.server_model }
+    difference = old_servers - new_servers
+    difference.each do |diff_model|
+      @server_threads.delete_if do |thread|
+        thread.server_model == diff_model
+      end
+    end
+  end
+
   # To change this template use File | Settings | File Templates.
 end
 
