ae0c292786e701a8e9ece032e20af1fae23ae0e1
facetostool <ivan.tugin@gmail.com>
2014-04-09 10:29:22 +0100

    remove digital ocean status

--- app/server/managers/ping_manager.rb
+++ app/server/managers/ping_manager.rb
@@ -2,9 +2,9 @@ module PingManager
 
   def start_pinging_server
     Thread.new do
-      @digital_ocean = DigitalOceanWrapper.new
+      #@digital_ocean = DigitalOceanWrapper.new
       while true
-        if @digital_ocean.get_droplet_by_name @server_model.name
+        #if @digital_ocean.get_droplet_by_name @server_model.name
           command = "ping -w1 -c1 #{@server_model.address}"
           r, w = IO.pipe
           pid = spawn(command, :out => w)
@@ -12,9 +12,9 @@ module PingManager
           w.close
           status = r.read.include?('0 received')
           @status = !status
-        else
-          @status = false
-        end
+        #else
+        #  @status = false
+        #end
         sleep TIME_FOR_UPDATE
       end
     end
