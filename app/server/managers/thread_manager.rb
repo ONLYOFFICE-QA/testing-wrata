3bf633754ed8e9a9809e562272cf9b32c66ed167
facetostool <ivan.tugin@gmail.com>
2014-04-17 18:06:58 +0100

    thread fail

--- app/server/managers/thread_manager.rb
+++ app/server/managers/thread_manager.rb
@@ -2,20 +2,22 @@ module ThreadManager
 
   def create_main_thread
     @main_thread = Thread.new(caller: method(__method__).owner.to_s) do
-      unless @test
-        Thread.stop
+      loop do
+        unless @test
+          Thread.stop
+        end
+        test_path = @test[:test_path]
+        location = @test[:location]
+        clear_log_file
+        test_options = ServerOptions.new(@test[:doc_branch], @test[:tm_branch], location.split(' ')[0], location.split(' ')[1])
+        full_start_command = start_test_on_server(test_path, test_options)    #MAIN FUNCTION
+        add_data_to_history(test_path, test_options, full_start_command, @client)
+        init_last_log
+        delete_log_file
+        delete_html_result
+        @last_log_end = 0
+        @test = nil
       end
-      test_path = @test[:test_path]
-      location = @test[:location]
-      clear_log_file
-      test_options = ServerOptions.new(@test[:doc_branch], @test[:tm_branch], location.split(' ')[0], location.split(' ')[1])
-      full_start_command = start_test_on_server(test_path, test_options)    #MAIN FUNCTION
-      add_data_to_history(test_path, test_options, full_start_command, @client)
-      init_last_log
-      delete_log_file
-      delete_html_result
-      @last_log_end = 0
-      @test = nil
     end
   end
 
