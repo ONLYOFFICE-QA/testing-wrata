f45b71b11b4ec0538c62ccfcf0625f2530487269
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-12 16:59:27 +0300

    remove duplicate `testing-shared` call

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require_relative '../../SharedFunctional/testing_shared'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
