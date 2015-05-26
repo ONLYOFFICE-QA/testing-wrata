d9ffab343795d47472356beb63449c56ff2f4e38
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-26 18:39:51 +0300

    fix first call of fixture

--- test/functional/servers_controller_test.rb
+++ test/functional/servers_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ServersControllerTest < ActionController::TestCase
   setup do
-    @user = server(:one)
+    @user = servers(:one)
   end
 
   test 'should get index' do
