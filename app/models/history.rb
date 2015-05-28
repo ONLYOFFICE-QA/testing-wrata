6ec1f3d2cc23d1bb49c5fa36c517d93c8c47a22b
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-28 11:27:30 +0300

    rename attribute from `user_id` to `server_id`

--- app/models/history.rb
+++ app/models/history.rb
@@ -3,5 +3,5 @@ class History < ActiveRecord::Base
   belongs_to :client
   has_one :start_option
 
-  attr_accessible :log, :file, :user_id, :client_id, :analysed, :total_results
+  attr_accessible :log, :file, :server_id, :client_id, :analysed, :total_results
 end
