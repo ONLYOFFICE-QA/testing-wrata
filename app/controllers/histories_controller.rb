135502b0edb85fb2c994592c3ad4990efa5edb94
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-28 11:22:26 +0300

    add CRUD to histories controller

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,11 +1,68 @@
 class HistoriesController < ApplicationController
-  # DELETE /history/1
+  before_action :set_history, only: [:show, :edit, :update, :destroy]
+
+  # GET /histories
+  # GET /histories.json
+  def index
+    @histories = History.all
+  end
+
+  # GET /histories/1
+  # GET /histories/1.json
+  def show
+  end
+
+  # GET /histories/new
+  def new
+    @history = History.new
+  end
+
+  # GET /histories/1/edit
+  def edit
+  end
+
+  # POST /histories
+  # POST /histories.json
+  def create
+    @history = History.new(history_params)
+
+    respond_to do |format|
+      if @history.save
+        format.html { redirect_to @history, notice: 'History was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @history }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @history.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /histories/1
+  # PATCH/PUT /histories/1.json
+  def update
+    respond_to do |format|
+      if @history.update(history_params)
+        format.html { redirect_to @history, notice: 'History was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @history.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /histories/1
+  # DELETE /histories/1.json
   def destroy
     @history = History.find(params[:id])
     @history.destroy
 
-    render nothing: true
+    respond_to do |format|
+      format.html { redirect_to histories_url }
+      format.json { head :no_content }
+    end
   end
+
 
   def set_analysed
     @history = History.find(params['id'])
@@ -25,4 +82,15 @@ class HistoriesController < ApplicationController
 
     render layout: false
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_history
+      @history = History.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def history_params
+      params[:history]
+    end
 end
