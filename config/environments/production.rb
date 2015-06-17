98b12341a282fd4272b0ef91319cebfb28f54ec6
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-17 13:43:34 +0300

    remove deprecated `config.serve_static_assets`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Runner::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
