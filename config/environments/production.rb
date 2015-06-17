b35712be0ad3da08e6a6a1120ea9cdb89a608ebf
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-17 17:13:50 +0300

    debug level on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Runner::Application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
