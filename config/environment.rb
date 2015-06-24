8cc69c5a6e7b78fa2595fde2d72a9fb675171b9b
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-24 12:01:28 +0300

    Redefine Logger Too more look like LoggerHelper

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Runner::Application.initialize!
+
+class Logger
+  def format_message(_severity, _timestamp, _progname, msg)
+    "#{Time.now.strftime('%T/%d.%m.%y')} (#{$PID}) #{msg}\n"
+  end
+end
