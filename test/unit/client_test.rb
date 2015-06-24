08a1255eaa6266e66e3416e934dbd2a54c7de1e1
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-24 10:50:44 +0300

    Remove unused comments from Unit Tests

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/unit/stroke_test.rb
+++ test/unit/stroke_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class StrokeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
