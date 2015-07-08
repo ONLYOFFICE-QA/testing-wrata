095a997caccb5816c9a9c42e2d8410c634608d72
Pavel Lobashov <shockwavenn@gmail.com>
2015-07-08 15:50:02 +0300

    Rename image to 'nct-at-stable'

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,5 +1,5 @@
 class ServersController < ApplicationController
-  EXECUTOR_IMAGE_NAME = 'nct-at-develop'
+  EXECUTOR_IMAGE_NAME = 'nct-at-stable'
 
   before_action :create_digital_ocean, only: [:cloud_server_create, :cloud_server_destroy]
 
