6d386e81bc58e843eb0e81ffb1c34f9aaa04c9a9
Pavel Lobashov <shockwavenn@gmail.com>
2015-07-13 13:24:58 +0300

    Truly remove all active-admin leftovers. Need db:clean.

--- db/migrate/20150710080626_remove_all_active_admin_leftovers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAllActiveAdminLeftovers < ActiveRecord::Migration
-  def change
-    drop_table :active_admin_comments
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710080626) do
+ActiveRecord::Schema.define(version: 20150528122354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
