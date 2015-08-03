ca093fc0b38e0d3de774414d112e0ba32eb68e59
Pavel Lobashov <shockwavenn@gmail.com>
2015-08-03 10:31:39 +0300

    Fix forgotten if, because of it - always failed to start delayed runs

--- app/workers/run_thread_manager.rb
+++ app/workers/run_thread_manager.rb
@@ -32,7 +32,7 @@ module RunThreadManager
       # @test_list = run.client.test_lists.find_by_name()
       # manager.add_test(tests, branch, location)
     when 'test_list'
-      fail NoMethodError, 'You cannot add run to queue with empty name'
+      fail NoMethodError, 'You cannot add run to queue with empty name' if run.name.empty?
       test_list = run.client.test_lists.find_by_name(run.name)
       names = test_list.test_files.inject([]) do |arr, test_file|
         arr << test_file.name
