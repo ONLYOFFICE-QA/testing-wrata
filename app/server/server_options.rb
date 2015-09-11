580fa38e62861773de6592f7d717f1fc4a583765
Pavel Lobashov <shockwavenn@gmail.com>
2015-09-11 11:12:03 +0300

    Replace @create_portal_region only once

--- app/server/server_options.rb
+++ app/server/server_options.rb
@@ -34,9 +34,7 @@ class ServerOptions
       region_command +=
         " && sed -i \\\"s/@create_portal_domain = '.info'/@create_portal_domain = '.#{@portal_type}'/g\\\" #{portal_data_docs} && " \
         "sed -i \\\"s/@create_portal_domain = '.com'/@create_portal_domain = '.#{@portal_type}'/g\\\" #{portal_data_docs} && "  \
-        "sed -i \\\"s/@create_portal_region = 'us'/@create_portal_region = '#{@portal_region}'/g\\\" #{portal_data_docs} && " \
-        "sed -i \\\"s/@create_portal_region = 'eu'/@create_portal_region = '#{@portal_region}'/g\\\" #{portal_data_docs} && " \
-        "sed -i \\\"s/@create_portal_region = 'sg'/@create_portal_region = '#{@portal_region}'/g\\\" #{portal_data_docs} && " \
+        "sed -i \\\"s/@create_portal_region = '.*'/@create_portal_region = '#{@portal_region}'/g\\\" #{portal_data_docs} && " \
         "sed -i \\\"s/@@portal_type = '.info'/@@portal_type = '.#{@portal_type}'/g\\\" #{portal_data_teamlab} && " \
         "sed -i \\\"s/@@portal_type = '.com'/@@portal_type = '.#{@portal_type}'/g\\\" #{portal_data_teamlab} && "  \
         "sed -i \\\"s/@@server_region = 'us'/@@server_region = '#{@portal_region}'/g\\\" #{portal_data_teamlab} && " \
