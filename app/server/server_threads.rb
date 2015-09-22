f3cf6466701882edee5d6967640a5dac4bbf6c51
Pavel Lobashov <shockwavenn@gmail.com>
2015-09-22 17:12:41 +0300

    Add log message about self-destruction process

--- app/server/server_threads.rb
+++ app/server/server_threads.rb
@@ -58,6 +58,7 @@ class ServerThreads < ActionController::Base
   def destroy_inactive_threads
     @server_threads.each do |server_thread|
       if server_thread.should_be_destroyed?
+        Rails.logger.info("O server: #{server_thread.server_model.name}, doomed to be destroyed")
         server_thread.server_model.cloud_server_destroy
       end
     end
