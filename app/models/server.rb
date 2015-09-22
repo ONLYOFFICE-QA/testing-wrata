710de7a23becbe4a3ca1e1eec37a96e4524df1e0
Pavel Lobashov <shockwavenn@gmail.com>
2015-09-22 15:55:29 +0300

    Duplicate `cloud_server_create` and `cloud_server_destroy` methods in model Need to remove them from controller later

--- app/models/server.rb
+++ app/models/server.rb
@@ -22,6 +22,26 @@ class Server < ActiveRecord::Base
     save
   end
 
+  def cloud_server_create
+    update_column(:_status, :creating)
+    begin
+      $digital_ocean.restore_image_by_name(EXECUTOR_IMAGE_NAME, name)
+    rescue => e
+      update_column(:_status, :destroyed)
+      raise e
+    end
+    $digital_ocean.wait_until_droplet_have_status(name)
+    new_address = $digital_ocean.get_droplet_ip_by_name(name)
+    update_column(:address, new_address)
+    update_column(:_status, :created)
+  end
+
+  def cloud_server_destroy
+    update_column(:_status, :destroying)
+    $digital_ocean.destroy_droplet_by_name(name)
+    update_column(:_status, :destroyed)
+  end
+
   def self.sort_servers(servers_array)
     servers_array.sort_by { |s| s.name[/\d+/].to_i }
   end
--- config.ru
+++ config.ru
@@ -4,5 +4,6 @@ require ::File.expand_path('../config/environment', __FILE__)
 run Runner::Application
 $delayed_runs = DelayedRunManager.new
 $run_managers = RunnerManagers.new
+$digital_ocean = DigitalOceanWrapper.new
 $threads = ServerThreads.new.init_threads
 $server_destroyer = ServerDestroyerWorker.new.start_thread
