2b33ecb7b79601d6abeb216bdd81649e8b84a79b
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-13 12:53:19 +0300

    Add duplicate method to model

--- app/models/delayed_run.rb
+++ app/models/delayed_run.rb
@@ -1,4 +1,19 @@
 class DelayedRun < ActiveRecord::Base
   belongs_to :client
   attr_accessible :name, :f_type, :method, :client_id, :start_time, :next_start
+
+  # Check if current delayed run should start in specific time
+  # @param time_to_run [ActiveSupport::TimeWithZone] time in which to check for start
+  # @return [True, False] should delayed run start in this time
+  def should_start_by_time?(time_to_run)
+    now = Time.now
+    run_datetime = time_to_run.to_time
+    if now.strftime('%d/%m/%y') == run_datetime.strftime('%d/%m/%y')
+      time_diff = (now - run_datetime).abs
+      Rails.logger.info "For delay run at #{time_to_run} time left #{time_diff} seconds"
+      (time_diff <= INFELICITY) || (run_datetime < now)
+    else
+      run_datetime < now
+    end
+  end
 end
