cb155900cb748454f69d74a7d3745ed1d3626202
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-23 12:56:26 +0300

    Revert incorrect commit data

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -27,4 +27,4 @@ TIME_FOR_UPDATE = 15
 REGION_LIST = ['info us', 'info eu', 'info sg', 'com us', 'com eu', 'com sg', 'com org', 'default']
 DEFAULT_REGION = 'default'
 SECURITY_PASSWORD = 'All love wheel'
-TIMEOUT_SERVER_SELFDESTROY = 5 * 60
+TIMEOUT_SERVER_SELFDESTROY = 60 * 60
