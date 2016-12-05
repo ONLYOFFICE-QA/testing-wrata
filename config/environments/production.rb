d57d6f1cc08a7af59e68e19b05c9b272fb8f87c4
Pavel Lobashov <shockwavenn@gmail.com>
2016-12-05 15:11:56 +0300

    Fix deprectation warning
    ```
    DEPRECATION WARNING: `config.serve_static_files` is deprecated and will be removed in Rails 5.1.
    ```

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,18 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true # TODO: disable as soon as all mystic errors are fixed
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = true
-
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like
-  # NGINX, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
