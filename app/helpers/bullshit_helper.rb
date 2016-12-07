26e8d1cc231f150ae2c490926da3eccd14ba1138
Pavel Lobashov <shockwavenn@gmail.com>
2016-12-07 16:24:01 +0300

    Remove bullshit helper

--- app/helpers/bullshit_helper.rb
+++
@@ -1,9 +0,0 @@
-module BullshitHelper
-  def self.match_minutes_and_hours(method)
-    hours = method.match(/_(\d*)_hours/)
-    hours = hours.captures.first if hours
-    minutes = method.match(/_(\d*)_minutes/)
-    minutes = minutes.captures.first if minutes
-    [hours, minutes]
-  end
-end
