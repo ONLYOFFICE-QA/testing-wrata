e39dd78972e4852bcc178f0d4a41ea99f5e85844
Pavel Lobashov <shockwavenn@gmail.com>
2017-06-05 12:37:45 +0300

    Use `net-ping` gem instead of sluggish native Linux exec

--- app/server/managers/ping_manager.rb
+++ app/server/managers/ping_manager.rb
@@ -1,21 +1,21 @@
 module PingManager
+  SSH_PORT = 22
+  PING_TIMEOUT = 1
+
   def start_pinging_server
     Thread.new(caller: method(__method__).owner.to_s) do
       loop do
         if @server_model._status == :destroyed || @server_model.address.nil?
           @status = false
         else
-          command = "ping -w1 -c1 #{@server_model.address}"
-          r, w = IO.pipe
-          pid = spawn(command, out: w)
-          Process.wait pid
-          w.close
-          Rails.logger.info "Executed '#{command}'"
-          status = r.read.include?('0 received')
-          @status = !status
+          @status = server_ssh_pinged?(@server_model.address)
         end
         sleep TIME_FOR_UPDATE
       end
     end
+  end
+
+  def server_ssh_pinged?(address)
+    Net::Ping::External.new(address, SSH_PORT, PING_TIMEOUT).ping?
   end
 end
