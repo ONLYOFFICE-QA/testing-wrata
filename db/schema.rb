9fca4c935df51af648ff6d511ec0e011d479f529
Pavel Lobashov <shockwavenn@gmail.com>
2017-09-25 13:08:52 +0300

    Add `SpecLanguage` to `ServerHistory` page

--- db/schema.rb
+++ db/schema.rb
@@ -10,104 +10,105 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413132631) do
+ActiveRecord::Schema.define(version: 20170925095418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "clients", force: :cascade do |t|
-    t.string   "login"
-    t.string   "password"
-    t.string   "first_name"
-    t.string   "second_name"
-    t.string   "post"
+  create_table "clients", id: :serial, force: :cascade do |t|
+    t.string "login"
+    t.string "password"
+    t.string "first_name"
+    t.string "second_name"
+    t.string "post"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.string   "project"
-    t.boolean  "verified",        default: false
-    t.index ["remember_token"], name: "index_clients_on_remember_token", using: :btree
+    t.string "password_digest"
+    t.string "remember_token"
+    t.string "project"
+    t.boolean "verified", default: false
+    t.index ["remember_token"], name: "index_clients_on_remember_token"
   end
 
-  create_table "delayed_runs", force: :cascade do |t|
-    t.string   "f_type"
+  create_table "delayed_runs", id: :serial, force: :cascade do |t|
+    t.string "f_type"
     t.datetime "start_time"
-    t.string   "name"
-    t.string   "method"
-    t.integer  "client_id"
+    t.string "name"
+    t.string "method"
+    t.integer "client_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "location"
+    t.string "location"
     t.datetime "next_start"
   end
 
-  create_table "histories", force: :cascade do |t|
-    t.string   "file"
-    t.integer  "server_id"
-    t.integer  "client_id"
+  create_table "histories", id: :serial, force: :cascade do |t|
+    t.string "file"
+    t.integer "server_id"
+    t.integer "client_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "log"
-    t.binary   "data"
-    t.string   "total_result"
+    t.text "log"
+    t.binary "data"
+    t.string "total_result"
     t.datetime "start_time"
   end
 
-  create_table "servers", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "address"
+  create_table "servers", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "comp_name"
-    t.string   "_status",               default: "destroyed"
-    t.integer  "book_client_id"
+    t.string "comp_name"
+    t.string "_status", default: "destroyed"
+    t.integer "book_client_id"
     t.datetime "last_activity_date"
-    t.boolean  "executing_command_now", default: false
-    t.boolean  "self_destruction",      default: true
-    t.string   "size",                  default: "1gb"
+    t.boolean "executing_command_now", default: false
+    t.boolean "self_destruction", default: true
+    t.string "size", default: "1gb"
   end
 
-  create_table "spec_languages", force: :cascade do |t|
-    t.string   "name"
+  create_table "spec_languages", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "start_options", force: :cascade do |t|
-    t.string  "docs_branch"
-    t.string  "teamlab_branch"
-    t.string  "shared_branch"
-    t.string  "teamlab_api_branch"
-    t.string  "portal_type"
-    t.string  "portal_region"
-    t.text    "start_command"
+  create_table "start_options", id: :serial, force: :cascade do |t|
+    t.string "docs_branch"
+    t.string "teamlab_branch"
+    t.string "shared_branch"
+    t.string "teamlab_api_branch"
+    t.string "portal_type"
+    t.string "portal_region"
+    t.text "start_command"
     t.integer "history_id"
+    t.string "spec_language"
   end
 
-  create_table "strokes", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "number"
-    t.integer  "test_file_id"
+  create_table "strokes", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "number"
+    t.integer "test_file_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "test_files", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "test_list_id"
+  create_table "test_files", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "test_list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "test_lists", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "client_id"
+  create_table "test_lists", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "client_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "branch"
-    t.string   "project"
+    t.string "branch"
+    t.string "project"
   end
 
 end
