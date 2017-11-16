3e11a40644b91e48a0fbe2292c1ea4d0f4afae50
Pavel Lobashov <shockwavenn@gmail.com>
2017-11-16 12:56:16 +0300

    Fix `none` as test progress in early stage of running test

--- app/server/server_thread.rb
+++ app/server/server_thread.rb
@@ -67,7 +67,7 @@ class ServerThread
       server_info[:test] = {
         name: slice_project_path(@test[:test_path]),
         location: @test[:location],
-        progress: @test_progress,
+        progress: @test_progress || 0,
         failed_count: @test_failed_count,
         time: testing_time,
         doc_branch: @test[:doc_branch],
