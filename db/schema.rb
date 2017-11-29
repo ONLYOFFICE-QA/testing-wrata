9fcf976ec4da0bc02e505a2cb33f85be8a27d30e
Pavel Lobashov <shockwavenn@gmail.com>
2017-11-29 17:58:48 +0300

    Remove not needed table

--- db/schema.rb
+++ db/schema.rb
@@ -118,10 +118,4 @@ ActiveRecord::Schema.define(version: 20171129132148) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tested_servers_lists", force: :cascade do |t|
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
