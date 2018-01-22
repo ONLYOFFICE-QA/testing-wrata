0be8c4d5d85a02e16e3d0f092d6e8faa44698aa1
Pavel Lobashov <ShockwaveNN@gmail.com>
2018-01-22 13:29:49 +0300

    Revert "Replace `Server_destroyer_worker` by `whenever` gem task"

--- config/schedule.rb
+++
@@ -1,6 +0,0 @@
-# docker workaround, see https://github.com/javan/whenever/issues/656#issuecomment-239111064
-ENV.each { |k, v| env(k, v) }
-
-every 1.minute do
-  runner 'Runner::Application.config.threads.destroy_inactive_servers', output: 'log/server_destroyer.log'
-end
