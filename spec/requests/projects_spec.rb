48416395a16c879f17bcfe6fd159176089af86fd
Pavel Lobashov <shockwavenn@gmail.com>
2018-11-13 16:02:12 +0300

    Fix test for redirect on forbidden pages

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Projects', type: :request do
   describe 'GET /projects' do
     it 'works! (now write some real specs)' do
       get projects_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
