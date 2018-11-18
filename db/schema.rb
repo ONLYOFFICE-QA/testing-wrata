1f26abbbb2ac9c61bcb3b40b13bbdf1f88aa267c
Pavel Lobashov <shockwavenn@gmail.com>
2018-11-18 16:04:02 +0300

    Add `secret_key_base` to rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181113122915) do
+ActiveRecord::Schema.define(version: 2018_11_13_122915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
