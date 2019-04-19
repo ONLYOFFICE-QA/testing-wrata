8e023184345a3e56c395d3e860f3d4200d4b5e2d
Pavel Lobashov <shockwavenn@gmail.com>
2019-04-19 11:44:44 +0300

    Fix inccorrect

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.allow_concurrency = true
-  config.mock_cloud_server = false
+  config.mock_cloud_server = true
 end
