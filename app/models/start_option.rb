ONLYOFFICE-QA/testing-wrata

commit 5034cacc26d541cc2f565c900f3543899304e5f8

Actualize RuboCop todo to 0.67.2 (#458)
* Add separate `rubocop-performance` gem

* Actualize RuboCop todo to 0.67.2

* Simplify StartOptions generator

* Simplify updated_data

app/models/start_option.rb
@@ -37,6 +37,16 @@ class StartOption < ActiveRecord::Base
     !(portal_type == 'info' || portal_type == 'com' || portal_type == 'default')
   end
 
+  # @param test [Hash] test with data to init
+  def init_from_test(test)
+    @docs_branch = test[:doc_branch]
+    @teamlab_branch = @test[:tm_branch]
+    @portal_type = @test[:location].split(' ')[0]
+    @portal_region = @test[:location].split(' ')[1]
+    @spec_browser = @test[:spec_browser]
+    @spec_language = @test[:spec_language]
+  end
+
   private
 
   # @param branch [String] branch to use

app/server/managers/thread_manager.rb
@@ -4,15 +4,9 @@ module ThreadManager
       loop do
         Thread.stop unless @test
         test_path = @test[:test_path]
-        location = @test[:location]
         clear_log_file
         @server_model.update_column(:executing_command_now, true)
-        test_options = StartOption.new(docs_branch: @test[:doc_branch],
-                                       teamlab_branch: @test[:tm_branch],
-                                       portal_type: location.split(' ')[0],
-                                       portal_region: location.split(' ')[1],
-                                       spec_browser: @test[:spec_browser],
-                                       spec_language: @test[:spec_language])
+        test_options = StartOption.new.init_from_test(@test)
         start_time = DateTime.now
         client = server_model.booked_client
         full_start_command = generate_full_start_command(test_path, test_options)

.rubocop.yml
@@ -1,3 +1,4 @@
+require: rubocop-performance
 inherit_from:
     - .rubocop_todo.yml
 AllCops:

.rubocop_todo.yml
@@ -1,6 +1,6 @@
 # This configuration was generated by
 # `rubocop --auto-gen-config`
-# on 2018-11-06 15:00:00 +0300 using RuboCop version 0.60.0.
+# on 2019-04-17 19:16:03 +0300 using RuboCop version 0.67.2.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
@@ -8,9 +8,9 @@
 
 # Offense count: 16
 Metrics/AbcSize:
-  Max: 34
+  Max: 31
 
-# Offense count: 8
+# Offense count: 10
 # Configuration parameters: CountComments, ExcludedMethods.
 # ExcludedMethods: refine
 Metrics/BlockLength:
@@ -27,6 +27,12 @@ Metrics/MethodLength:
   Max: 27
 
 # Offense count: 1
+# Configuration parameters: PreferredName.
+Naming/RescuedExceptionsVariableName:
+  Exclude:
+    - 'app/server/managers/html_result_manager.rb'
+
+# Offense count: 1
 Security/Open:
   Exclude:
     - 'app/server/managers/html_result_manager.rb'
@@ -39,13 +45,7 @@ Style/ClassAndModuleChildren:
   Exclude:
     - 'test/test_helper.rb'
 
-# Offense count: 1
-# Configuration parameters: AllowCoercion.
-Style/DateTime:
-  Exclude:
-    - 'app/server/managers/thread_manager.rb'
-
-# Offense count: 92
+# Offense count: 96
 Style/Documentation:
   Enabled: false
 
@@ -55,7 +55,7 @@ Style/MixinUsage:
     - 'bin/setup'
     - 'bin/update'
 
-# Offense count: 235
+# Offense count: 263
 # Configuration parameters: AllowHeredoc, AllowURI, URISchemes, IgnoreCopDirectives, IgnoredPatterns.
 # URISchemes: http, https
 Metrics/LineLength:

Gemfile
@@ -24,5 +24,6 @@ group :development, :test do
   gem 'rails-controller-testing'
   gem 'rspec-rails'
   gem 'rubocop'
+  gem 'rubocop-performance'
   gem 'scss_lint', require: false
 end

app/controllers/runner_controller.rb
@@ -101,20 +101,10 @@ class RunnerController < ApplicationController
     servers = JSON.parse(servers_json)
 
     output_json = {}
-    server_data = []
-
-    servers.each do |server|
-      next unless server['name'].is_a?(String)
-
-      server_thread = Runner::Application.config.threads.get_thread_by_name(server['name'])
-      next unless server_thread
-
-      server_data << server_thread.get_info_from_server(current_client, with_log: server['with_log'])
-    end
 
     manager = Runner::Application.config.run_manager.find_manager_by_client_login(current_client.login)
 
-    output_json[:servers_data] = server_data
+    output_json[:servers_data] = fill_server_data(servers)
     output_json[:queue_data] = {}
     output_json[:queue_data][:servers] = manager.booked_servers
     output_json[:queue_data][:tests] = manager.tests
@@ -151,5 +141,23 @@ class RunnerController < ApplicationController
     Runner::Application.config.threads.destroy_unbooked_servers
 
     render body: nil
+  end
+
+  private
+
+  # @param servers [Array<String>] list of servers to get data
+  # @return [Array<Hash>] data about servers
+  def fill_server_data(servers)
+    server_data = []
+    servers.each do |server|
+      next unless server['name'].is_a?(String)
+
+      server_thread = Runner::Application.config.threads.get_thread_by_name(server['name'])
+      next unless server_thread
+
+      server_data << server_thread.get_info_from_server(current_client, with_log: server['with_log'])
+    end
+
+    server_data
   end
 end

commit 019ef4eb0931943d0a25b1a0406a4fc4b21236a9

Fix setting test params
Broken in #458
https://github.com/ONLYOFFICE/testing-wrata/pull/458/files#diff-c90532a016560b85d0e3286e3837503eR41
Since this send only default params :(

app/models/start_option.rb
@@ -38,14 +38,13 @@ class StartOption < ActiveRecord::Base
   end
 
   # @param test [Hash] test with data to init
-  def init_from_test(test)
-    @docs_branch = test[:doc_branch]
-    @teamlab_branch = test[:tm_branch]
-    @portal_type = test[:location].split(' ')[0]
-    @portal_region = test[:location].split(' ')[1]
-    @spec_browser = test[:spec_browser]
-    @spec_language = test[:spec_language]
-    self
+  def self.from_test(test)
+    StartOption.new(docs_branch: test[:doc_branch],
+                    teamlab_branch: test[:tm_branch],
+                    portal_type: test[:location].split(' ')[0],
+                    portal_region: test[:location].split(' ')[1],
+                    spec_browser: test[:spec_browser],
+                    spec_language: test[:spec_language])
   end
 
   private

app/server/managers/thread_manager.rb
@@ -6,7 +6,7 @@ module ThreadManager
         test_path = @test[:test_path]
         clear_log_file
         @server_model.update_column(:executing_command_now, true)
-        test_options = StartOption.new.init_from_test(@test)
+        test_options = StartOption.from_test(@test)
         start_time = DateTime.now
         client = server_model.booked_client
         full_start_command = generate_full_start_command(test_path, test_options)

config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.allow_concurrency = true
-  config.mock_cloud_server = true
+  config.mock_cloud_server = false
 end