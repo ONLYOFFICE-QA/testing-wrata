ONLYOFFICE-QA/testing-wrata

commit be842efeb3d8d70b97fc790d249ff6a84b1a4598

Update RuboCop to `0.71.0` (#470)
* Update RuboCop to `0.71.0`

* Move `Naming/RescuedExceptionsVariableName`

Since bug in rubocop it should be stored in permanent config

* Fix RuboCop `Style/SafeNavigation` cop

* Fix RuboCop `Style/FrozenStringLiteralComment` cop

app/models/start_option.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class StartOption < ActiveRecord::Base
   belongs_to :history
 

commit 915d79842ec5db4dd574a21c1cca1c3a69a74f6c

Fix adding to Fronzen lyteral
Caused by #470

app/models/start_option.rb
@@ -58,7 +58,7 @@ class StartOption < ActiveRecord::Base
     projects = Project.all
 
     projects.each do |project|
-      command << project.update_git_command(branch)
+      command += project.update_git_command(branch)
     end
 
     command