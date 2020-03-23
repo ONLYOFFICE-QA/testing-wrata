1a3765e127a1f39d1299ba93c5877ef686666caa
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-23 15:06:11 +0300

    Fix delay_run_params unsued

--- app/controllers/delay_run_controller.rb
+++ app/controllers/delay_run_controller.rb
@@ -9,19 +9,19 @@ class DelayRunController < ApplicationController
   end
 
   def add_run
-    @run = Runner::Application.config.delayed_runs.add_run(delayed_run_params, current_client)
+    @run = Runner::Application.config.delayed_runs.add_run(params, current_client)
 
     render layout: false
   end
 
   def change_run
-    Runner::Application.config.delayed_runs.change_run(delayed_run_params)
+    Runner::Application.config.delayed_runs.change_run(params)
 
     render body: nil
   end
 
   def delete_run
-    Runner::Application.config.delayed_runs.delete_run(delayed_run_params['id'])
+    Runner::Application.config.delayed_runs.delete_run(params['id'])
 
     render body: nil
   end
@@ -41,9 +41,5 @@ class DelayRunController < ApplicationController
       flash[:empty_pages] = 'You need be authorized' # Not quite right!
       render signin_path
     end
-  end
-
-  def delayed_run_params
-    params.require(:delayed_run).permit(:name, :f_type, :method, :client_id, :start_time, :next_start)
   end
 end
