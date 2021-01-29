507999129aee4a9bdaf58da2af547f7c4161cd84
Pavel Lobashov <shockwavenn@gmail.com>
2021-01-29 14:41:42 +0300

    Actualize rubocop todo for 1.9.0

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -2,11 +2,7 @@
 
 module ServersHelper
   def test_fail?(total_result)
-    if total_result.include?(SUCCESS_TEST_STR)
-      false
-    else
-      true
-    end
+    total_result.exclude?(SUCCESS_TEST_STR)
   end
 
   def test_result(total_result)
