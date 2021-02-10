08985ff61a9849073a269b8d7c5d034d8d330fdc
Pavel Lobashov <shockwavenn@gmail.com>
2021-02-10 16:40:16 +0300

    Add more

--- spec/views/histories/show_html_results.erb_spec.rb
+++ spec/views/histories/show_html_results.erb_spec.rb
@@ -3,9 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe 'histories/show_html_results', type: :view do
+  before do
+    @rspec_result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html('spec/views/histories/html_result.html')
+  end
+
   it 'Show do not have file message if @rspec_result is empty' do
+    @rspec_result = nil
     render
 
     expect(rendered).to match(/Don't have html file of this test!/)
+  end
+
+  it 'Contains percentage of completed test' do
+    render
+
+    expect(rendered).to match(/100.0%/)
+  end
+
+  it 'Contains at least single describe-child' do
+    render
+
+    expect(rendered).to match(/describe-child/)
+  end
+
+  it 'Contains at least single example-name' do
+    render
+
+    expect(rendered).to match(/example-name/)
   end
 end
