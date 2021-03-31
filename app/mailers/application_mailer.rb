b79b47ac0737070464fee6e06d07ce79e231fff4
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-03-31 18:50:41 +0300

    Correct `from` field for failure messages (#970)
    This may help with problem sending notifications to gmail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'notifications@wrata.onlyoffice.com'
+  default from: Rails.application.credentials.failure_notification_username
   layout 'mailer'
 end
