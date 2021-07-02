e5052459f5e8e39872ce7d2ea387108e9040a2c0
Pavel Lobashov <shockwavenn@gmail.com>
2021-07-02 17:20:13 +0300

    Fix logging in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,5 +77,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.logger = Logger.new($stdout)
+  logger = ActiveSupport::Logger.new($stdout)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
 end
