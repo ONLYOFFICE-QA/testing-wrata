81859599906d98263df7cc85651601a44ef0812b
Pavel Lobashov <shockwavenn@gmail.com>
2021-09-07 23:25:17 +0300

    Fix rubocop `RSpec/InstanceVariable` cop

--- spec/models/spec_language_spec.rb
+++ spec/models/spec_language_spec.rb
@@ -3,12 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe SpecLanguage, type: :model do
-  before do
-    @spec_lang = described_class.create!(name: 'Lang1')
-  end
+  let(:spec_lang) { described_class.create!(name: 'Lang1') }
 
   it 'When there are already a same spec lang' do
-    duplicate = @spec_lang.dup
+    duplicate = spec_lang.dup
     expect(duplicate).not_to be_valid
   end
 end
--- spec/views/spec_browsers/edit.html.erb_spec.rb
+++ spec/views/spec_browsers/edit.html.erb_spec.rb
@@ -3,16 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe 'spec_browsers/edit', type: :view do
-  before do
-    @spec_browser = assign(:spec_browser, SpecBrowser.create!(
-                                            name: 'MyString'
-                                          ))
+  let(:spec_browser) do
+    assign(:spec_browser, SpecBrowser.create!(
+                            name: 'MyString'
+                          ))
   end
 
   it 'renders the edit spec_browser form' do
     render
 
-    assert_select 'form[action=?][method=?]', spec_browser_path(@spec_browser), 'post' do
+    assert_select 'form[action=?][method=?]', spec_browser_path(spec_browser), 'post' do
       assert_select 'input[name=?]', 'spec_browser[name]'
     end
   end
