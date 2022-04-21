9735877301c396ac38402367a0a31b47c5082947
Pavel Lobashov <shockwavenn@gmail.com>
2022-04-21 13:33:52 +0300

    Force headless driver for all tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe 'the sign in process', type: :feature, js: true do
+describe 'the sign in process', type: :feature do
   it 'signing in with fake user result in failure' do
     visit '/sessions/new'
     fill_in('Login', with: 'test@example.com')
