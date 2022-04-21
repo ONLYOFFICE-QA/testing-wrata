b306cdfe96971382659add88b31f53fead5f904a
Pavel Lobashov <shockwavenn@gmail.com>
2022-04-21 14:38:13 +0300

    Add more tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,4 +8,13 @@ describe 'the sign in process', type: :feature do
     click_button('Log in')
     expect(page).to have_content 'Invalid login/password'
   end
+
+  it 'siging in with created user result in success' do
+    client = Client.create(login: 'user@example.com', password: 'password')
+    visit '/sessions/new'
+    fill_in('Login', with: client.login)
+    fill_in('Password', with: client.password)
+    click_button('Log in')
+    expect(page).not_to have_content 'Invalid login/password'
+  end
 end
