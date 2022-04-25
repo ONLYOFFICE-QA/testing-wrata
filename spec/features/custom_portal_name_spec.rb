07322c975adbecdf083ac00375dbcbd0d068c840
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-04-25 16:11:00 +0300

    Add `WebDriver` tests with `capybara` (#1339)
    * Add `WebDriver` tests wit `capybara`
    
    * Remove extra line
    
    * Add firefox\geckdriver installation
    
    * Fix command
    
    * Force headless driver for all tests
    
    * Change only JS driver
    
    * Add more tests
    
    * Add first working test for window

---
+++ spec/features/custom_portal_name_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+describe 'custom portal name window', type: :feature, js: true do
+  it 'calling custom portal name will show portal name input' do
+    client = Client.create(login: 'user@example.com', password: 'password')
+    visit '/sessions/new'
+    fill_in('Login', with: client.login)
+    fill_in('Password', with: client.password)
+    click_button('Log in')
+    select('custom', from: 'portal-list')
+    expect(page).to have_selector('.bootbox', visible: :visible)
+  end
+end
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+describe 'the sign in process', type: :feature do
+  it 'signing in with fake user result in failure' do
+    visit '/sessions/new'
+    fill_in('Login', with: 'test@example.com')
+    fill_in('Password', with: '12345678')
+    click_button('Log in')
+    expect(page).to have_content 'Invalid login/password'
+  end
+
+  it 'siging in with created user result in success' do
+    client = Client.create(login: 'user@example.com', password: 'password')
+    visit '/sessions/new'
+    fill_in('Login', with: client.login)
+    fill_in('Password', with: client.password)
+    click_button('Log in')
+    expect(page).not_to have_content 'Invalid login/password'
+  end
+end
