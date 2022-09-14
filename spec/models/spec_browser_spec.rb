534ef8a68adaa0ac597258a8a6a4f27a6866db4c
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-14 19:13:07 +0300

    Add some more tests

--- spec/models/spec_browser_spec.rb
+++ spec/models/spec_browser_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe SpecBrowser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with some name' do
+    expect(described_class.new(name: 'chrome')).to be_valid
+  end
+
+  it 'is not valid with default attributes' do
+    expect(described_class.new).not_to be_valid
+  end
+
+  it 'is not valid if any numbers in name' do
+    expect(described_class.new(name: '1')).not_to be_valid
+  end
+
+  it 'is not valid if any special symbols in name' do
+    expect(described_class.new(name: '-$')).not_to be_valid
+  end
 end
