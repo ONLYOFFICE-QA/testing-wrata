01bb80c7d26453261e0b4e02a319d9b41509b7b2
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-16 20:23:46 +0300

    Add autogenerated spec

--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -2,51 +2,116 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Clients', type: :request do
-  new_client_data = { client: { login: 'new_login',
-                                password: 'new_pass',
-                                password_confirmation: 'new_pass' } }
-  it 'GET new' do
-    get(new_client_path)
-    expect(response).to render_template(:new)
+RSpec.describe '/clients', type: :request do
+  let(:valid_attributes) do
+    { login: 'User1', password: '123' }
   end
 
-  it 'GET index' do
-    get(clients_path)
-    expect(response).to render_template(:index)
+  let(:invalid_attributes) do
+    { login: 'User2' }
   end
 
-  it 'Creating a client' do
-    expect do
-      post(clients_path,
-           params: new_client_data)
-    end.to change(Client, :count).by(1)
-
-    expect(response).to redirect_to(runner_path)
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      Client.create! valid_attributes
+      get clients_url
+      expect(response).to be_successful
+    end
   end
 
-  it 'Show a client' do
-    get("#{clients_path}/1")
-    expect(response).to render_template('clients/show')
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      client = Client.create! valid_attributes
+      get client_url(client)
+      expect(response).to be_successful
+    end
   end
 
-  it 'Get an edit page' do
-    get(edit_client_path(1))
-    expect(response).to render_template('clients/edit')
+  describe 'GET /new' do
+    it 'renders a successful response' do
+      get new_client_url
+      expect(response).to be_successful
+    end
   end
 
-  it 'Post an update to client' do
-    put("#{clients_path}/1",
-        params: { id: 1 }.merge(new_client_data))
-    expect(response).to redirect_to(client_path(1))
+  describe 'GET /edit' do
+    it 'renders a successful response' do
+      client = Client.create! valid_attributes
+      get edit_client_url(client)
+      expect(response).to be_successful
+    end
   end
 
-  it 'Destroy a client' do
-    expect do
-      delete("#{clients_path}/1",
-             params: { id: 1 })
-    end.to change(Client, :count).by(-1)
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Client' do
+        expect do
+          post clients_url, params: { client: valid_attributes }
+        end.to change(Client, :count).by(1)
+      end
 
-    expect(response).to redirect_to(clients_path)
+      it 'rediret to root afterr creating' do
+        post clients_url, params: { client: valid_attributes }
+        expect(response).to redirect_to(runner_path)
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Client' do
+        expect do
+          post clients_url, params: { client: invalid_attributes }
+        end.not_to change(Client, :count)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post clients_url, params: { client: invalid_attributes }
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { login: 'User3', password: '123' }
+      end
+
+      it 'updates the requested client' do
+        client = Client.create! valid_attributes
+        patch client_url(client), params: { client: new_attributes }
+        client.reload
+        expect(client[:name]).to eq(new_attributes[:name])
+      end
+
+      it 'redirects to the client' do
+        client = Client.create! valid_attributes
+        patch client_url(client), params: { client: new_attributes }
+        client.reload
+        expect(response).to redirect_to(client_url(client))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it "renders a successful response (i.e. to display the 'edit' template)" do
+        client = Client.create! valid_attributes
+        patch client_url(client), params: { client: invalid_attributes }
+        expect(response).not_to be_successful
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'destroys the requested client' do
+      client = Client.create! valid_attributes
+      expect do
+        delete client_url(client)
+      end.to change(Client, :count).by(-1)
+    end
+
+    it 'redirects to the clients list' do
+      client = Client.create! valid_attributes
+      delete client_url(client)
+      expect(response).to redirect_to(clients_url)
+    end
   end
 end
