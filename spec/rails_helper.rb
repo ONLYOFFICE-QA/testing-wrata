d3d3b0d32c6b3d1611a9ef7b0075126d207944a6
OnlyOffice Testing Robot <teamlab.ruby@gmail.com>
2023-11-17 12:49:35 +0200

    Actualize rspec init (#1805)
    * Actualize rspec init
    
    * Fix failing tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,12 +30,11 @@ require 'capybara/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+  abort e.to_s.strip
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.fixture_path = Rails.root.join('spec/fixtures').to_s
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -57,7 +56,7 @@ RSpec.configure do |config|
   #     end
   #
   # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
+  # https://rspec.info/features/6-0/rspec-rails
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
