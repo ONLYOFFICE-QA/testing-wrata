eda6aa45caf16592eb97c1b09f4c3cf2d56e4a95
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-01-19 11:55:27 +0200

    Actualize `rails_helper` to fix Rails warnings
    This fix #1890
    Config fixed via running `rails generate rspec:install`

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,7 +26,7 @@ Capybara.javascript_driver = :selenium_headless
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# Rails.root.glob('spec/support/**/*.rb').sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -37,7 +37,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join('spec/fixtures').to_s
+  config.fixture_paths = [
+    Rails.root.join('spec/fixtures')
+  ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
