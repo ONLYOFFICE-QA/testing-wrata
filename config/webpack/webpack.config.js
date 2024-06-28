676bd20e39c54a3d52a583e38b0b293519dffed3
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-06-28 14:00:07 +0300

    Add babel

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,20 +1,29 @@
-const webpack = require("webpack")
+const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, '..', '..', 'app/assets/builds')
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+    mode: "production",
+    devtool: "source-map",
+    entry: {
+        application: "./app/javascript/application.js"
+    },
+    output: {
+        filename: "[name].js",
+        sourceMapFilename: "[file].map",
+        chunkFormat: "module",
+        path: path.resolve(__dirname, '..', '..', 'app/assets/builds')
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+};
